3f3962fee439fef820beeecac8b6898187bb424c
prasanth4a5 <ummadi1994@gmail.com>
2024-01-09 22:35:47 +0530

    roboshop-terraform

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "tags" {}
+variable "env" {}
+variable "lb_type" {}
+variable "internal" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "sg_port" {}
+variable "subnets" {}
+variable "acm_certificate_arn" {}
