6bd0df2a1d6412f83675619c1baa7f8422f6263c
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-07 16:26:43 +0530

    Test

--- main.tf
+++ main.tf
@@ -9,6 +9,42 @@ resource "aws_lb" "main" {
   tags = merge(local.tags, { Name = "${var.env}-alb"})
 }
 
+resource "aws_lb_listener" "main" {
+    load_balancer_arn = aws_lb.main.arn
+    port              = var.internal ? 80 : 443
+    protocol          = var.internal ? "HTTP" : "HTTPS"
+#    ssl_policy        = var.internal ? null : "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = var.internal ? null : var.acm_certificate_arn
+
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "ERROR"
+        status_code  = "404"
+      }
+    }
+  }
+
+  resource "aws_lb_listener" "frontend" {
+    count             = var.internal ? 0 : 1
+    load_balancer_arn = aws_lb.main.arn
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+      type = "redirect"
+
+      redirect {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
+
+
 resource "aws_security_group" "main" {
   name        = "${var.env}-alb-sg"
   description = "${var.env}-alb-sg"
