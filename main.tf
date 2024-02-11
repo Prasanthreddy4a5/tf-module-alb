4f620ff7ba2faacba0fe520578fb320d7d8e6368
Prasanthreddy4a5 <ummadi1994@gmail.com>
2024-02-11 14:07:58 +0530

    Test

--- main.tf
+++ main.tf
@@ -29,22 +29,7 @@ resource "aws_lb_listener" "main" {
     }
   }
 
-resource "aws_lb_listener" "frontend" {
-  count             = var.internal ? 0 : 1
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
 
-    default_action {
-      type = "redirect"
-
-      redirect {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-  }
 
 
 resource "aws_security_group" "main" {
@@ -66,6 +51,7 @@ resource "aws_security_group" "main" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
